a647c6da7c260e48d9b0882161ebceadc26f736b
jitender <jindjind123>
2021-03-03 10:42:13 +0530

    Add TwoNumberSum program

---
+++ app/src/main/java/com/ds/TowNumberSum.java
@@ -0,0 +1,22 @@
+package com.ds;
+
+class TowNumberSum {
+
+    public static int[] twoNumberSum(int[] array, int targetSum) { // My Code
+        for(int i=0; i< array.length-1; i++){
+            int fNum = array[i];
+            for(int j=i+1; j<array.length; j++){
+                int lNum = array[j];
+                if(fNum + lNum == targetSum){
+                    return  new int[] {fNum, lNum};
+                }
+            }
+        }
+        return new int[0];
+    }
+    public static void main(String[] args) {
+
+        int[] ar = {1,3,5};
+        System.out.println(twoNumberSum(ar,3));
+    }
+}
